009e794dcd19880761fe29bb1451161b3c3fb875
Adam Germain <55671330+Ryacuda@users.noreply.github.com>
2023-11-08 16:05:55 +0100

    tile datas and map manager

---
+++ Assets/Tiles/MapManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapManager : MonoBehaviour
+{
+    [SerializeField] private Tilemap map;
+    [SerializeField] private List<TileData> tileDatas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
